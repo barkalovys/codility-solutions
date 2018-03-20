e80cfff7ead873a0f965a5e3fb096bf6e138ac65
barkalov <barkalovys@gmail.com>
2018-03-20 16:02:56 +0300

    Triangle solution added

---
+++ solution/triangle.go
@@ -0,0 +1,18 @@
+package solution
+
+import "sort"
+   
+type Desc []int
+func (A Desc) Len() int { return len(A) }
+func (A Desc) Swap(i, j int)      { A[i], A[j] = A[j], A[i] }
+func (A Desc) Less(i, j int) bool { return A[i] > A[j] }
+
+func Triangle(A []int) int {
+    sort.Sort(Desc(A))
+    for i := 0; i < len(A) - 2; i++ {
+        if A[i] < A[i+1] + A[i+2] {
+            return 1
+        } 
+    }
+    return 0
+}
